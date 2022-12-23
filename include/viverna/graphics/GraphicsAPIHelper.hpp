de808fc24c82eefc556fbaac255f3b21aa0c6878
MicPret <pretamichele0@gmail.com>
2022-12-23 14:52:57 +0100

    fixed include order for glad+GLFW

--- include/viverna/graphics/GraphicsAPIHelper.hpp
+++ include/viverna/graphics/GraphicsAPIHelper.hpp
@@ -2,11 +2,11 @@
 #define VERNA_GRAPHICS_API_HELPER_HPP
 
 #if defined(VERNA_ANDROID)
-#include <EGL/egl.h>
 #include <GLES3/gl32.h>
+#include <EGL/egl.h>
 #elif defined(VERNA_DESKTOP)
-#include <GLFW/glfw3.h>
 #include <glad/gl.h>
+#include <GLFW/glfw3.h>
 #endif
 
 namespace verna {
