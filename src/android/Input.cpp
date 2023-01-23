d00380a9476c4b83a51006804d78a825be08cd1a
MicPret <pretamichele0@gmail.com>
2023-01-23 17:10:54 +0100

    android check input 2

--- src/android/Input.cpp
+++ src/android/Input.cpp
@@ -31,7 +31,6 @@ static void GetTouchPos(unsigned& x, unsigned& y) {
 static int32_t HandleInput(struct android_app* app, AInputEvent* event) {
     float x, y;
     auto event_type = AInputEvent_getType(event);
-    VERNA_LOGI(std::to_string(event_type == AINPUT_EVENT_TYPE_MOTION));
     switch (event_type) {
         case AINPUT_EVENT_TYPE_MOTION:
             SetTouch((AMotionEvent_getAction(event) & AMOTION_EVENT_ACTION_MASK)
@@ -39,6 +38,7 @@ static int32_t HandleInput(struct android_app* app, AInputEvent* event) {
             x = AMotionEvent_getX(event, 0);
             y = AMotionEvent_getY(event, 0);
             SetTouchPos(static_cast<uint32_t>(x), static_cast<uint32_t>(y));
+            VERNA_LOGI(std::to_string(touch));
             return 1;
         default:
             return 0;
