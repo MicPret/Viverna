0ad8f4d98786dea413e9eac740a96c1d77e4726a
MicPret <pretamichele0@gmail.com>
2023-02-24 15:59:47 +0100

    Fixed game folder
    Fixed contents in src/game

--- src/game/Application.cpp
+++ src/game/Application.cpp
@@ -1,5 +1,8 @@
 #include <game/core/Application.hpp>
 
+#include <viverna/core/Input.hpp>
+#include <viverna/graphics/Renderer.hpp>
+
 namespace verna {
 void OnAppResume(VivernaState& app_state) {
     // Called when the application (re)starts
@@ -9,6 +12,14 @@ void OnAppPause(VivernaState& app_state) {
 }
 void OnAppUpdate(VivernaState& app_state, DeltaTime<float, Seconds> dt) {
     // Called every frame (remember to call Draw()!)
+
+    KeyListener escape(Key::Escape);
+    if (escape.Pressed()) {
+        app_state.SetFlag(VivernaState::RUNNING_FLAG, false);
+        return;
+    }
+
+    Draw();
 }
 
 }  // namespace verna
