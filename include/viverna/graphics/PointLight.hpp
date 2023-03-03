abc212023db61f80850e1c4d4f88beae65a13f07
MicPret <pretamichele0@gmail.com>
2023-03-03 17:08:04 +0100

    Added support for point lights
    Warning: there is still no API to add lights to the scene

---
+++ include/viverna/graphics/PointLight.hpp
@@ -0,0 +1,22 @@
+#ifndef VERNA_POINT_LIGHT_HPP
+#define VERNA_POINT_LIGHT_HPP
+
+#include <viverna/maths/Vec3f.hpp>
+
+namespace verna {
+struct PointLight {
+    struct Attenuation {
+        float constant;
+        float linear;
+        float quadratic;
+    };
+
+    Vec3f position;
+    Vec3f ambient;
+    Vec3f diffuse;
+    Vec3f specular;
+    Attenuation attenuation;
+};
+}  // namespace verna
+
+#endif
