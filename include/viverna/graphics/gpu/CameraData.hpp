fe7d74717fc19f1cc1f340529d35c20a080e0cc3
MicPret <pretamichele0@gmail.com>
2023-03-28 13:27:10 +0200

    Better gpu::CameraData
    Added near, far, aspect_ratio, fovy; added constructors

--- include/viverna/graphics/gpu/CameraData.hpp
+++ include/viverna/graphics/gpu/CameraData.hpp
@@ -1,6 +1,7 @@
 #ifndef VERNA_GPU_CAMERA_DATA_HPP
 #define VERNA_GPU_CAMERA_DATA_HPP
 
+#include <viverna/graphics/Camera.hpp>
 #include <viverna/maths/Mat4f.hpp>
 
 namespace verna::gpu {
@@ -8,6 +9,20 @@ struct CameraData {
     Mat4f projection_matrix;
     Mat4f view_matrix;
     Mat4f pv_matrix;
+    float near;
+    float far;
+    float aspect_ratio;
+    float fovy;
+    CameraData() = default;
+    inline CameraData(const Camera& cam) :
+        projection_matrix(cam.GetProjectionMatrix()),
+        view_matrix(cam.GetViewMatrix()),
+        near(cam.near_plane),
+        far(cam.far_plane),
+        aspect_ratio(cam.aspect_ratio),
+        fovy(cam.fovy) {
+        pv_matrix = projection_matrix * view_matrix;
+    }
 };
 }  // namespace verna::gpu
 
