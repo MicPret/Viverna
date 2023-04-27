e37ed9afd088f9ab7dda0caeec633cafb50dad13
MicPret <pretamichele0@gmail.com>
2023-04-27 15:51:29 +0200

    Added DIFFUSE_INDEX and SPECULAR_INDEX
    Texture maps can now be accessed this way: material.textures[Material::DIFFUSE_INDEX]

--- include/viverna/graphics/Material.hpp
+++ include/viverna/graphics/Material.hpp
@@ -8,6 +8,9 @@ namespace verna {
 struct Material {
     std::array<TextureId, 8> textures = {};
     std::array<float, 4> parameters = {};
+
+    static constexpr size_t DIFFUSE_INDEX = 0;
+    static constexpr size_t SPECULAR_INDEX = 1;
 };
 }  // namespace verna
 
