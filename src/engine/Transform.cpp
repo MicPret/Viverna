2a027feb5cdbbfa098c695ce54f70cf26ebd1493
MicPret <pretamichele0@gmail.com>
2023-05-03 19:12:53 +0200

    Second attempt at implementing Transform::LookAt

--- src/engine/Transform.cpp
+++ src/engine/Transform.cpp
@@ -1,8 +1,18 @@
 #include <viverna/core/Transform.hpp>
 
+#include <cmath>
+
 namespace verna {
-void Transform::LookAt(const Vec3f& pos) {
-    Mat4f rot = Mat4f::LookAt(position, position + Forward(), Vec3f::UnitY());
-    rotation = Quaternion(rot);
+void Transform::LookAt(const Vec3f& pos, const Vec3f& up) {
+    Vec3f forward = up.x < 0.99999f ? Vec3f::UnitX().Cross(up).Normalized()
+                                    : Vec3f::UnitY();
+    Vec3f to_pos = (pos - position).Normalized();
+    Vec3f axis = forward.Cross(to_pos);
+    float cosangle = forward.Dot(to_pos);
+    if (cosangle < 0.99999f) {
+        float angle =
+            cosangle <= -1.0f ? verna::maths::Pi() : std::acos(cosangle);
+        rotation = Quaternion(axis.Normalized(), angle);
+    }
 }
 }  // namespace verna
