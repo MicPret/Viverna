3bb510dfc2a459936de6eb3da08bda1ddf090220
MicPret <pretamichele0@gmail.com>
2023-05-04 13:25:30 +0200

    Safer Transform::LookAt

--- src/engine/Transform.cpp
+++ src/engine/Transform.cpp
@@ -4,14 +4,20 @@
 
 namespace verna {
 void Transform::LookAt(const Vec3f& target) {
+    constexpr float epsilon = 1e-5;
+    constexpr float one_minus_epsilon = 1.0f - epsilon;
     Vec3f forward = Forward();
-    Vec3f to_target = (target - position).Normalized();
+    Vec3f to_target = target - position;
+    float mag = to_target.Magnitude();
+    if (mag < epsilon)
+        return;
+    to_target = (1.0f / mag) * to_target;
     float cosangle = forward.Dot(to_target);
-    if (cosangle < 0.99999f) {
-        float angle =
-            cosangle <= -1.0f ? verna::maths::Pi() : std::acos(cosangle);
-        rotation =
-            Quaternion(forward.Cross(to_target).Normalized(), angle) * rotation;
-    }
+    if (cosangle >= one_minus_epsilon)
+        return;
+    float angle = cosangle <= -one_minus_epsilon ? verna::maths::Pi()
+                                                 : std::acos(cosangle);
+    rotation =
+        Quaternion(forward.Cross(to_target).Normalized(), angle) * rotation;
 }
 }  // namespace verna
